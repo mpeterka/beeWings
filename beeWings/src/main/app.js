ef4b5ba03d8e3eb78312c35d47c5648c8f45a5ba
mpeterka <martin.peterka@gmail.com>
2013-05-02 10:04:57 +0200

    + flot plots for results

--- beeWings/src/main/app.js
+++ beeWings/src/main/app.js
@@ -1,8 +1,10 @@
-//http://vigorbee.cz/files/kridla-manual.pdf
+// http://vigorbee.cz/files/kridla-manual.pdf
 var c07;
 var p0, p7, p3;
 var c12, c24;
 var p1, p2, p4;
+
+var results = [];
 
 var getPosition = function(obj) {
 	return $(obj).offset();
@@ -24,7 +26,7 @@ jsPlumb.ready(function() {
 	jsPlumb.importDefaults({
 		DragOptions : { cursor: "pointer", zIndex:2000 },
 		ConnectionsDetachable: false,
-		Anchors: ["AutoDefault"],
+		Anchors: ["Center"],
 		Connector: ["Straight"],
 		Endpoint: ["Blank"],
 		EndpointsOnTop: false,
@@ -38,13 +40,14 @@ jsPlumb.ready(function() {
 	
 	var endpointOptions = { 
 	}; 
-	p0 = jsPlumb.addEndpoint('p0', { anchor:"LeftMiddle" }, endpointOptions );
-	p3 = jsPlumb.addEndpoint('p3', { anchor:"AutoDefault" }, endpointOptions );
-	p7 = jsPlumb.addEndpoint('p7', { anchor:"RightMiddle" }, endpointOptions );
+	p0 = jsPlumb.addEndpoint('p0', { anchor:"LeftMiddle"}, endpointOptions);
+	p3 = jsPlumb.addEndpoint('p3', { anchor:"AutoDefault"}, endpointOptions );
+	p7 = jsPlumb.addEndpoint('p7', { anchor:"RightMiddle"}, endpointOptions );
 	
 	var c07 = jsPlumb.connect({ 
 		source: p0,
-		target: p7
+		target: p7,
+		paintStyle: {strokeStyle: "blue"}
 	});
 	
 	
@@ -67,11 +70,75 @@ jsPlumb.ready(function() {
 	jsPlumb.getSelector(".point").bind("beforeDrag", function(connection){
 		console.log(connection);
 	});
+	
+
+	$(".point").draggable({
+		stop : function() {
+			refreshLoketniIndex();
+		}
+	});
 
 });
 
+var refreshLoketniIndex = function() {
+	$("#loketniIndex").text(loketniIndex());
+};
+
+var refreshPlot = function() {
+	var plotOptions = {
+		series : {
+			lines : {
+				show : false
+			},
+			points : {
+				show : true,
+				fillColor: "red"
+			}
+		},
+		xaxis: {
+			tickSize: 5,
+			min: -10.0,
+			max: +10.0
+		},
+		yaxis: {
+			tickSize: 0.5,
+			min: 0.5,
+			max: 4.0
+		}
+	};
+	
+	var $placeholder = $("#graph");
+	
+	var plot = $.plot($placeholder, [results], plotOptions);
+
+	var ctx = plot.getCanvas().getContext("2d");
+
+	// carnica
+	drawRegion(ctx, plot.pointOffset({ x: 0, y: 2}), plot.pointOffset({ x: 10, y: 4}), "rgba(0, 0, 165, 0.2)", "rgba(0, 0, 165, 1)");
+	
+	// caucasia
+	drawRegion(ctx, plot.pointOffset({ x: -10, y: 1.7}), plot.pointOffset({ x: 0, y: 3.2}), "rgba(0, 165, 0, 0.2)", "rgba(0, 165, 0, 1)");
+	
+	// mellifera
+	drawRegion(ctx, plot.pointOffset({ x: -10, y: 0.5}), plot.pointOffset({ x: 2, y: 2.1}), "rgba(127, 127, 0, 0.2)", "rgba(127, 127, 0, 1)");
+};
+
+var drawRegion = function(ctx, posBottom, posTop, fillColor, shadowColor) {
+	ctx.shadowBlur = 10;
+	ctx.shadowColor = shadowColor;
+	ctx.fillStyle = fillColor;
+	
+	ctx.beginPath();
+	ctx.moveTo(posBottom.left, posBottom.top);
+	ctx.lineTo(posTop.left, posBottom.top);
+	ctx.lineTo(posTop.left, posTop.top);
+	ctx.lineTo(posBottom.left, posTop.top);
+	ctx.fill();
+};
+
 $(document).ready(function() {
-	$("#calculateBtn").click(function() {
-		$("#loketniIndex").text(loketniIndex());
+	$("#addBtn").click(function() {
+		results.push([0, loketniIndex()]);
+		refreshPlot();
 	});
 });
